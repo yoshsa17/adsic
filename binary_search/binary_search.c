d7f8b83fea2fccab29c75bb062c9c8d9d6ffca24
Yoshiro Saito <saiorsin44den@gmail.com>
2022-06-14 13:52:48 +0900

    refactor binary_serch file

--- binary_search/binary_search.c
+++ binary_search/binary_search.c
@@ -1,33 +1,32 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
 int binary_search(int key, int array[], int size);
 int binary_search_recursive(int key, int *array, int min, int max);
 
-void run_all_test();
+void run_all_tests();
 void test_binary_search();
 void test_binary_search_recursive();
 
-////// Main
+// Main
 
-int main(int argc, char **argv){
-  run_all_test();
+int main(int argc, char **argv) {
+  run_all_tests();
   return 0;
 }
 
+// Functions
 
-////// Functions
-
-int binary_search(int key, int *array, int size){
+int binary_search(int key, int *array, int size) {
   int low = 0;
   int mid = 0;
   int high = size - 1;
 
-  while(low <= high) {
+  while (low <= high) {
     mid = low + (high - low) / 2;
-    if(array[mid] < key){
+    if (array[mid] < key) {
       low = mid + 1;
-    } else if(key < array[mid]) {
+    } else if (key < array[mid]) {
       high = mid - 1;
     } else {
       return mid;
@@ -38,15 +37,15 @@ int binary_search(int key, int *array, int size){
 }
 
 int binary_search_recursive(int key, int array[], int min, int max) {
-  if(max < min){
+  if (max < min) {
     return -1;
   }
 
   int midpoint = min + (max - min) / 2;
 
-  if(array[midpoint] < key) {
+  if (array[midpoint] < key) {
     return binary_search_recursive(key, array, midpoint + 1, max);
-  } else if (key < array[midpoint]){
+  } else if (key < array[midpoint]) {
     return binary_search_recursive(key, array, min, midpoint - 1);
   } else {
     return midpoint;
@@ -55,13 +54,13 @@ int binary_search_recursive(int key, int array[], int min, int max) {
 
 ////// Test
 
-void run_all_test(){
+void run_all_tests() {
   test_binary_search_recursive();
   test_binary_search();
   printf("OK\n");
 }
 
-void test_binary_search(){
+void test_binary_search() {
   const int size = 10;
   int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   assert(binary_search(2, array, size) == 1);
@@ -70,7 +69,7 @@ void test_binary_search(){
   assert(binary_search(12, array, size) == -1);
 }
 
-void test_binary_search_recursive(){
+void test_binary_search_recursive() {
   const int size = 10;
   int max = size - 1;
   int array[] = {23, 34, 55, 99, 244, 435, 8534, 8888, 13232, 19939};

471f00b337986551dc20f03fec7fac8325c3cb54
Yoshiro Saito <saiorsin44den@gmail.com>
2022-06-14 13:54:59 +0900

    add bst

--- binary_search_tree/binary_search_tree.c
+++ binary_search_tree/binary_search_tree.c
@@ -1,6 +1,10 @@
+#include <assert.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 typedef struct node BstNode;
 struct node {
@@ -12,22 +16,43 @@ struct node {
 BstNode *bst_create_new_node(int data);
 BstNode *bst_insert(BstNode *root, int data);
 bool bst_search(BstNode *root, int data);
+BstNode *bst_find_node(BstNode *root, int data);
+BstNode *bst_get_min(BstNode *root);
+int bst_get_height(BstNode *root);
+void bst_print_value(BstNode *root);
+bool bst_get_is_bst(BstNode *root);
+BstNode *bst_delete_value(BstNode *root, int data);
 
-////// Main
+void run_all_tests();
+void test_insert();
+void test_search();
+void test_get_min();
+void test_get_height();
+void test_get_is_bst();
+void test_delete_value();
+
+// Main
 
 int main() {
-  BstNode *root = NULL;
-  root = bst_insert(root, 100);
+  run_all_tests();
   return 0;
 }
 
-////// Functions
+// Functions
 
 BstNode *bst_create_new_node(int data) {
   BstNode *new_node = calloc(1, sizeof(BstNode));
   new_node->data = data;
   new_node->left = new_node->right = NULL;
   return new_node;
+}
+
+void bst_destroy_tree(BstNode *root) {
+  if (root == NULL) return;
+
+  bst_destroy_tree(root->left);
+  bst_destroy_tree(root->right);
+  free(root);
 }
 
 BstNode *bst_insert(BstNode *root, int data) {
@@ -55,7 +80,7 @@ bool bst_search(BstNode *root, int data) {
 
 BstNode *bst_find_node(BstNode *root, int data) {
   if (root == NULL) {
-    printf("ERROR: tree is empty");
+    printf("Error: tree is empty");
     exit(EXIT_FAILURE);
   } else if (data < root->data) {
     root->left = bst_delete_value(root->left, data);
@@ -66,69 +91,23 @@ BstNode *bst_find_node(BstNode *root, int data) {
   }
 }
 
-int bst_find_min(BstNode *root) {
+BstNode *bst_get_min(BstNode *root) {
   if (root == NULL) {
     printf("Error: Tree is empty\n");
-    return -1;
+    exit(EXIT_FAILURE);
   } else if (root->left == NULL) {
-    return root->data;
+    return root;
   }
-  return bst_find_min(root->left);
+  return bst_get_min(root->left);
 }
 
-int bst_find_height(BstNode *root) {
+int bst_get_height(BstNode *root) {
   if (root == NULL) return -1;
   // + 1 is for root height
-  return max(bst_find_height(root->left), bst_find_height(root->right)) + 1;
+  return MAX(bst_get_height(root->left), bst_get_height(root->right)) + 1;
 }
 
-bool bst_get_is_bst(BstNode *root, int minValue, int maxValue) {
-  if (root == NULL) return true;
-
-  if (root->data < minValue && root->data > maxValue &&
-      bst_get_is_bst(root->left, minValue, root->data) &&
-      bst_get_is_bst(root->right, root->data, maxValue)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-BstNode *bst_delete_value(BstNode *root, int data) {
-  if (root == NULL) {
-    return root;
-  } else if (data < root->data) {
-    root->left = bst_delete_value(root->left, data);
-  } else if (data > root->data) {
-    root->right = bst_delete_value(root->right, data);
-  } else {
-    // Case 1: No child
-    if (root->left == NULL && root->right == NULL) {
-      free(root);
-      root = NULL;
-    }
-
-    // Case 2: 1 child
-    else if (root->left == NULL) {
-      BstNode *temp = root;
-      root = root->right;
-      free(temp);
-    } else if (root->right == NULL) {
-      BstNode *temp = root;
-      root = root->left;
-      free(temp);
-    }
-
-    // Case 3: 2 children
-    else {
-      BstNode *temp = bst_find_min(root->right);
-      root->data = temp->data;
-      root->right = bst_delete_value(root->right, temp->data);
-    }
-  }
-  return root;
-}
-
+// print values with Depth-first traversal (In-order)
 void bst_print_value(BstNode *root) {
   if (root == NULL) {
     return;
@@ -139,6 +118,57 @@ void bst_print_value(BstNode *root) {
   bst_print_value(root->right);
 }
 
+bool is_between(BstNode *root, int minValue, int maxValue) {
+  if (root == NULL) return true;
+
+  return root->data > minValue && root->data < maxValue &&
+         is_between(root->left, minValue, root->data) &&
+         is_between(root->right, root->data, maxValue);
+}
+
+bool bst_get_is_bst(BstNode *root) {
+  if (root == NULL) return true;
+
+  return is_between(root, INT_MIN, INT_MAX);
+}
+
+BstNode *bst_delete_value(BstNode *root, int data) {
+  if (root == NULL) {
+    return root;
+  } else if (data < root->data) {
+    root->left = bst_delete_value(root->left, data);
+  } else if (data > root->data) {
+    root->right = bst_delete_value(root->right, data);
+  } else {
+    // Case 1: No child : delete the node
+    if (root->left == NULL && root->right == NULL) {
+      free(root);
+      root = NULL;
+    }
+
+    // Case 2: 1 child : delete the node and connect the parent and the child
+    else if (root->left == NULL) {
+      BstNode *temp = root;
+      root = root->right;
+      free(temp);
+    } else if (root->right == NULL) {
+      BstNode *temp = root;
+      root = root->left;
+      free(temp);
+    }
+
+    // Case 3: 2 children : 1. find min in right or find max in left
+    //                      2. copy the value
+    //                      3. delete duplicate
+    else {
+      BstNode *temp = bst_get_min(root->right);
+      root->data = temp->data;
+      root->right = bst_delete_value(root->right, temp->data);
+    }
+  }
+  return root;
+}
+
 BstNode *bst_get_successor(BstNode *root, int data) {
   BstNode *cur = bst_find_node(root, data);
   if (cur == NULL) {
@@ -147,7 +177,7 @@ BstNode *bst_get_successor(BstNode *root, int data) {
 
   // Case 1: Node has right subtree
   if (cur->right != NULL) {
-    return bst_find_min(cur->right);
+    return bst_get_min(cur->right);
   }
 
   // Case 2: No right subtree
@@ -163,4 +193,69 @@ BstNode *bst_get_successor(BstNode *root, int data) {
       }
     }
   }
+}
+
+// Functions
+
+void run_all_tests() {
+  test_insert();
+  test_search();
+  test_get_min();
+  test_get_height();
+  test_get_is_bst();
+  test_delete_value();
+  printf("OK\n");
+}
+
+BstNode *create_tree() {
+  BstNode *root = NULL;
+  root = bst_insert(root, 30);
+  root = bst_insert(root, 10);  // min
+  root = bst_insert(root, 50);
+  root = bst_insert(root, 232);  // max
+  root = bst_insert(root, 40);
+  root = bst_insert(root, 20);
+  root = bst_insert(root, 32);
+  return root;
+}
+
+void test_insert() {
+  BstNode *root = create_tree();
+  // bst_print_value(root);
+  bst_destroy_tree(root);
+}
+
+void test_search() {
+  BstNode *root = create_tree();
+  assert(bst_search(root, 50) == true);
+  assert(bst_search(root, 11) == false);
+  bst_destroy_tree(root);
+}
+
+void test_get_min() {
+  BstNode *root = create_tree();
+  assert(bst_get_min(root)->data == 10);
+  bst_destroy_tree(root);
+}
+
+void test_get_height() {
+  BstNode *root = create_tree();
+  // printf("Height: %d\n", bst_get_height(root));
+  assert(bst_get_height(root) == 3);
+  bst_destroy_tree(root);
+}
+
+void test_get_is_bst() {
+  BstNode *root = create_tree();
+  assert(bst_get_is_bst(root) == true);
+  bst_destroy_tree(root);
+}
+
+void test_delete_value() {
+  BstNode *root = create_tree();
+  // bst_print_value(root);
+  // printf("-----------\n");
+  bst_delete_value(root, 20);
+  // bst_print_value(root);
+  bst_destroy_tree(root);
 }
